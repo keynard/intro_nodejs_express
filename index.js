ed87376e851c82a068b422d048e317fc5adc7932
unknown <YY301@ccs.local>
2025-02-06 05:27:29 -0800

    my 50% of my work

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Serve static files form the "public" folder
+app.use(express.static('public'));
+
+// Define a route for the home page 
+    app.get('/About', (req,res)=>{
+        res.send('About us');
+    });
+
+app.use(express.json());//Middleware
+
+app.post('/submit', (req,res)=>{
+    const data = req.body;
+    res.send(`Receivced: ${JSON.stringify(data)}`)
+});
+//Start the server
+app.listen(port,()=>{
+    console.log (`Server running at http://localhost:${port}`);
+
+});
+
